aaaa8f32e8f28e74dae9a4852117b33128a9b7f8
remruatpuii1 <76507276+remruatpuii1@users.noreply.github.com>
2021-01-04 15:17:30 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -4,11 +4,11 @@ var speed,weight,thickness;
 function setup() {
   createCanvas(1600,400);
   bullet=createSprite(50, 200, 50, 50);
-  wall=createSprite(1200,200,thickness,height/2);
-  wall.shapeColor=color(80,80,80);
   speed=random(223,321);
   weight=random(30,52);
   thickness=random(22,83);
+  wall=createSprite(1200,200,thickness,height/2);
+  wall.shapeColor=color(80,80,80);
 }
 
 function draw() {
